626b4f59d9c4d42a5752a9b9d3e58a1acaf731ea
Sasikaran Shamosihan <shamosihanaiengeneer@outlook.com>
2024-11-16 19:35:15 +0530

    Add files via upload
    this is very useful project for python beginners and math's solvers

---
+++ circle's area and perimeter finder.py
@@ -0,0 +1,7 @@
+r=float(input("enter circle's radian :-"))
+def area_of_circle(rad):
+    return 22/7*rad*rad
+def perimeter_of_circle(rad):
+    return 2*22/7*rad
+print("circle's area ="+ str(area_of_circle(r)))
+print("circle's peimeter ="+str(perimeter_of_circle(r)))
